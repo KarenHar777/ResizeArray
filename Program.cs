84ac70d218f1f0adf84f1806801926db437559d4
KarenHarutyunyan <karenvardanovich@gmail.com>
2023-01-06 01:52:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Resize(ref int[] array, int value, int index)
+    {
+        int[] newArray = new int[array.Length + 1];
+
+        newArray[index] = value;
+
+        for (int i = 0; i < index; i++)
+        {
+            newArray[i] = array[i];
+        }
+
+        for (int i = index; i < array.Length; i++)
+        {
+            newArray[i + 1] = array[i];
+        }
+        array = newArray;
+    }
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4 };
+
+        Resize(ref myArray, 5, 2);
+    }
+}
